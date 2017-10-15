e20f1414ce4876e053f2cb0aa39353f1ef39ab3d
Dan Choe <devdanchoe@gmail.com>
2017-10-15 06:21:05 -0400

    next

--- myvenv/c.py
+++ myvenv/c.py
@@ -1,16 +1,77 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 14 21:54:40 2017
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
-@author: danna
-"""
+import plotly.plotly as py
+import plotly.graph_objs as go
 
-url = "https://v3v10.vitechinc.com/solr/policy_info/select?indent=on&q=*:*&wt=json"
 
-def getmarket():
-    
-    page = urllib2.urlopen("http://bitcoincharts.com/t/markets.json")
-    data = json.load(page)
-    for elem in data:
-        if elem['symbol'] == "mtgoxUSD":
-            print(elem)
+accidental = 0
+non_accidental = 0
+
+app = dash.Dash()
+
+colors = {
+    'background': '#eee',
+    'text': '#000'
+}
+
+
+header = "default"
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='HackRU Data Visualization',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+    html.Div(children='''
+        Vitech
+    '''),
+    dcc.Graph(
+        id='example-graph1',
+        figure={
+            'data': [
+                {'x': [accidental,non_accidental], 'y': [100, 10], 'type': 'bar', 'name': 'Dental'},
+                {'x': [accidental,non_accidental], 'y': [5, 24], 'type': 'bar', 'name': 'Accidental'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    ),
+    dcc.Graph(
+        id='example-graph3',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [20, 23, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [92, 24, 35], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    ),
+    dcc.Graph(
+        id='example-graph4',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [100, 23, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [92, 24, 35], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- myvenv/test.py
+++ myvenv/test.py
@@ -10,8 +10,8 @@ import urllib.request
 import ijson
 import urllib.request as urlopen
 from matplotlib import pyplot as plt
-
-from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
+from datetime import time
+from flask import Flask, flash, redirect, render_template, request
 from flask_restful import Api
 
 app = Flask(__name__)
@@ -20,6 +20,10 @@ api = Api(app)
 url = "https://v3v10.vitechinc.com/solr/policy_info/select?indent=on&q=*:*&wt=json"
 url_part = "https://v3v10.vitechinc.com/solr/participant/select?indent=on&q=*:*&wt=json"
 url_ad = "https://v3v10.vitechinc.com/solr/activities/select?indent=on&q=*:*&wt=json"
+
+stateDict = {}
+adsType = []
+adsCnt = []
 
 #@app.route('url',methods=['GET'])
 @app.route("/")
@@ -37,7 +41,7 @@ def index():
     plt.legend(['Data1','Data2'])
     plt.xlabel('this is x axis')
     plt.ylabel('this is y axis')
-    plt.show()
+#    plt.show()
     #export img in same directory
     #plt.savefig('exproted_img')
     return "hi"
@@ -45,24 +49,22 @@ def index():
 @app.route("/product")
 def product():
     product = '&rows=0&fl=numFound&fq=insurance_product:'
-    labels = ['Dental','Accidental']
+    labelss = ['Dental','Accidental']
     numParticipants = []
     
-    for label in labels:
+    for label in labelss:
         data = urllib.request.urlopen(url+product+label)
         response = data.read().decode('utf-8')
         content = json.loads(response)
         results = content['response']
-        numParticipants.append(results['numFound'])
+        numParticipants.append(int(results['numFound']))
 
-    plt.pie(numParticipants, labels=labels)
-    plt.show()
+#    plt.pie(numParticipants, labels=labels)
+#    plt.show()
     
-    return "product"
-    #return render_template('chart.html', set=zip(values, labels, colors))
+    #return "product"
+    return render_template('donut_chart.html', values=[100,200], labels=labelss, legend='product')
 
-#currID = 0
-stateDict = {}
 
 def loadInfoF(currID):
     participants = url_part + '&fq=id:' + str(currID) + '&fl=state'
@@ -111,15 +113,35 @@ def state(id):
     return "state" + str(id)
 
 
-@app.route("/stateProduct")
-def stateProduct():
+@app.route("/ads")
+def ads():
+    product = '&fl=activity_type,targeted_counts'
+    rows = '&rows=10'
+    legend = 'Marketing Campaign'
+    
+    f = urllib.request.urlopen(url_ad+rows+product)
+   
+    for prefix, event, value in ijson.parse(f):
+        if prefix == 'response.docs.item.targeted_counts':
+            adsCnt.append(value)        
+            print(prefix, ' ', event, ' ', value)
+            
+        elif prefix == 'response.docs.item.activity_type':
+            print(prefix, ' ', event, ' ', value)
+            adsType.append(value)
+    
+    return render_template('line_chart.html', values=adsType, labels=adsCnt, legend=legend)
+
+
+@app.route("/stateProduct/<int:id>")
+def stateProduct(id):
     product = '&fl=participant_id&fq=insurance_product:'
     labels = ['Dental','Accidental']
 #    rows = '&rows=1482000'
-    rows = '&rows=50'
-    
+    rows = '&rows=100'
+    legend = labels[int(id)-1] + 'products popularity accross state'
     # For All Participants (policy_info)
-    f = urllib.request.urlopen(url+rows+product+labels[1])
+    f = urllib.request.urlopen(url+rows+product+labels[int(id)-1])
    
     # For check info of individual    
     count = 0
@@ -139,34 +161,14 @@ def stateProduct():
     for key, value in stateDict.items():
         print('stateDict key: ', key,' value: ', value)
     
-    return "state"
+    return render_template('line_chart.html', values=list(stateDict.values()), labels=list(stateDict.keys()), legend=legend)
 
-    
-# class HelloWorld(Resource):
-#     def get(self):
-#         return {'hello': 'world'}
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-# api.add_resource(HelloWorld, '/')    
-
-#@app.route("/hello")
-#def hello():
-#    return "Hello World!"
-
-@app.route("/hello/<string:name>/")
-def hello(name):
-    return render_template(
-        'test.html',name=name)
- 
-@app.route("/members")
-def members():
-    return "Members"
- 
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name
  
 if __name__ == "__main__":
-    #app.run()
     app.run(host='0.0.0.0', port=80)
     
 #    
